e22b24dcdbe36821a50d0679dab877b2cb94b6df
pablodiegoss <pablodiegoss@hotmail.com>
2017-06-19 13:52:08 -0300

    adjust new background

--- game/src/game_scene.cpp
+++ game/src/game_scene.cpp
@@ -49,7 +49,7 @@ void GameScene::verifyWinOrLose(){
 
 void GameScene::load(){
     Player* player = new Player("assets/sprites/paperboy2.png", 400, 500, 40,60);
-    Background* background = new Background("assets/sprites/background.png", 0,0, 640,480);
+    Background* background = new Background("assets/sprites/background.png", 0,-4200, 4000,5000);
     hud = new Text("Score", "assets/fonts/font.ttf", 30, false, new Color(100,100,100,255), new Color(0,0,0,0));
 
     gameObjectsList.push_back(player);
